604dabddf2655d686eaa18810311fe14e27124f2
shraddhesh singh <sids@protonmail.com>
2020-08-26 22:28:59 +0000

    changed

---
+++ dotnet3.1-imagebuilder/notice.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if [ "$(shuf -i 1-20 -n 1)" -eq 1 ]; then
+  echo '
+                   ***** NOTICE *****
+        This image is being created by Shraddhesh Singh.
+An official `microsoft/dotnet:sdk` image to run the `dotnet` tool exists at
+https://hub.docker.com/r/microsoft/dotnet.
+
+                ***** END OF NOTICE *****
+'
+fi
+dotnet "$@"
